6f1a5c7196f83eafd4e212a43315cfab157fdc97
Lapo Luchini <lapo@lapo.it>
2014-02-15 10:02:41 +0000

    Fix copy-paste error in copyright.

--- int10.js
+++ int10.js
@@ -1,5 +1,5 @@
-// Big integer printing library
-// Copyright (c) 2008-2014 Lapo Luchini <lapo@lapo.it>
+// Big integer base-10 printing library
+// Copyright (c) 2014 Lapo Luchini <lapo@lapo.it>
 
 // Permission to use, copy, modify, and/or distribute this software for any
 // purpose with or without fee is hereby granted, provided that the above
