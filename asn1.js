8988295d5f1cb405df8d8a9a79cdc5611fa80c96
Lapo Luchini <lapo@lapo.it>
2014-02-15 09:36:20 +0000

    Already checked in the outer "if".

--- asn1.js
+++ asn1.js
@@ -410,7 +410,7 @@ ASN1.decode = function (stream) {
         // must have valid content
         getSub();
     } else if (tag.isUniversal() && ((tag.tagNumber == 0x03) || (tag.tagNumber == 0x04))) {
-        if (tag.isUniversal() && tag.tagNumber == 0x03) stream.get(); // skip BitString unused bits, must be in [0, 7]
+        if (tag.tagNumber == 0x03) stream.get(); // skip BitString unused bits, must be in [0, 7]
         // sometimes BitString and OctetString do contain ASN.1
         try {
             getSub();
