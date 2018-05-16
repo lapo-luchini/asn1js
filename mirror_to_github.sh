ac6ebd187a43fe8707e3175bdaa2c105f79f53b6
Lapo Luchini <lapo@lapo.it>
2018-05-16 07:21:13 +0000

    Automatically reset marks when creating new repository.

--- mirror_to_github.sh
+++ mirror_to_github.sh
@@ -3,6 +3,7 @@ cd `dirname "$0"`
 if [ ! -d .git ]; then
     git init
     git remote add origin git@github.com:lapo-luchini/asn1js.git
+    rm git-marks1.txt git-marks2.txt
 fi
 touch git-marks1.txt git-marks2.txt
 mtn --quiet --authors=git-authors.txt --branches-file=git-branches.txt --import-marks=git-marks1.txt --export-marks=git-marks1.txt git_export | \
