96c61de81f2f105d1a5fa4eaaaf547129e6014d5
Lapo Luchini <lapo@lapo.it>
2021-12-13 18:17:47 +0000

    Download RFCs in a subdirectory.

--- updateRFC.sh
+++ updateRFC.sh
@@ -11,8 +11,9 @@ downloadRFC() {
     fi
 }
 echo '{}' > rfcasn1.json # start from scratch
+mkdir -p rfc
 for n in 5280 3369 3161; do
-    downloadRFC $n
-    ./parseRFC.js rfc$n.txt
+    ( cd rfc ; downloadRFC $n )
+    ./parseRFC.js rfc/rfc$n.txt
 done
 echo Conversion completed.
