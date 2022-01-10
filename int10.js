a7d63f1a1d8d6bd6a9b47466ca7c7862c6dd30c1
Lapo Luchini <lapo@lapo.it>
2022-01-10 21:39:21 +0000

    Update copyright year.

--- int10.js
+++ int10.js
@@ -1,5 +1,5 @@
 // Big integer base-10 printing library
-// Copyright (c) 2008-2021 Lapo Luchini <lapo@lapo.it>
+// Copyright (c) 2008-2022 Lapo Luchini <lapo@lapo.it>
 
 // Permission to use, copy, modify, and/or distribute this software for any
 // purpose with or without fee is hereby granted, provided that the above
