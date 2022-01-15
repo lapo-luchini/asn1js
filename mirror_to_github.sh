00122a448406a802a24ee126e73b05a150e3119e
Lapo Luchini <lapo@lapo.it>
2022-01-15 23:53:39 +0000

    Improve git export with automatic creation and update of authors and branches file.

--- mirror_to_github.sh
+++ mirror_to_github.sh
@@ -7,6 +7,8 @@ if [ ! -d .git ]; then
     rm git-marks1.txt git-marks2.txt
 fi
 touch git-marks1.txt git-marks2.txt
+[ -f git-authors.txt ] || echo 'lapo@lapo.it = Lapo Luchini <lapo@lapo.it>' > git-authors.txt
+mtn ls branches --ignore-suspend-certs | sort -V | awk -F . '{ print $0 " = " ($4 ? $4 : "trunk") }' > git-branches.txt
 mtn --quiet --authors=git-authors.txt --branches-file=git-branches.txt --import-marks=git-marks1.txt --export-marks=git-marks1.txt git_export | \
     git fast-import --import-marks=git-marks2.txt --export-marks=git-marks2.txt
 git push --mirror origin
