398defae076eb59056558d16f6c88e6340c486a3
Lapo Luchini <lapo@lapo.it>
2022-10-27 20:32:39 +0000

    More generic type searching. Still working with `Certificate`.

--- dumpASN1.js
+++ dumpASN1.js
@@ -10,22 +10,31 @@ const
     colBlue = "\x1b[34m",
     colReset = "\x1b[0m";
 
+function searchType(name) {
+    for (const r of Object.values(rfc))
+        if (name in r.types) {
+            // console.log(name + ' found in ' + r.name);
+            return r.types[name];
+        }
+    throw 'Type not found: ' + name;
+}
+
 function translate(def) {
     if (def?.type?.type)
         def = def.type;
     while (def?.type == 'defined') {
         const name = def.name;
-        try {
-            def = rfc['1.3.6.1.5.5.7.0.18'].types[name].type;
-        } catch (e) {
-            throw 'Type not found: ' + name;
-        }
+        def = searchType(name).type;
     }
     return def ?? {};
 }
 
-function print(value, def, indent) {
+function print(value, def, stats, indent) {
     if (indent === undefined) indent = '';
+    if (stats) {
+        stats.total ??= 0;
+        stats.recognized ??= 0;
+    }
     // console.log(def);
     let deftype = translate(def);
     let tn = value.typeName();
@@ -40,8 +49,10 @@ function print(value, def, indent) {
     }
     if (tn.replaceAll('_', ' ') != deftype.name && deftype.name != 'ANY')
         def = null;
+    if (stats) ++stats.total;
     let name = '';
     if (def) {
+        if (stats) ++stats.recognized;
         if (def.id) name += colBlue + def.id + colReset;
         if (def.type == 'defined') name = (name ? name + ' ' : '') + def.name;
         if (name) name += ' ';
@@ -71,20 +82,22 @@ function print(value, def, indent) {
                     let tn = subval.typeName().replaceAll('_', ' ');
                     do {
                         type = deftype.content[j++];
-                    } while (('optional' in type || 'default' in type) && type.name != tn);
+                    } while (type && ('optional' in type || 'default' in type) && type.name != tn);
                 }
             }
-            s += print(subval, type, indent);
+            s += print(subval, type, stats, indent);
         }
     }
     return s;
 }
 
-let content = fs.readFileSync(process.argv[2]);
+let content = fs.readFileSync(process.argv[3]);
 try { // try PEM first
     content = Base64.unarmor(content);
 } catch (e) { // try DER/BER then
 }
 let result = ASN1.decode(content);
 content = null;
-console.log(print(result, rfc['1.3.6.1.5.5.7.0.18'].types.Certificate));
+let stats = {};
+console.log(print(result, searchType(process.argv[2]), stats));
+console.log('Stats:', (stats.recognized * 100 / stats.total).toFixed(2) + '%');
