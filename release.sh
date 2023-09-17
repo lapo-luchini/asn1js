408efbc4a18c786b843995746d86165c61680e80
Lapo Luchini <lapo@lapo.it>
2023-09-17 10:59:06 +0000

    Favicon fixes.

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 set -e
 FILES="
   asn1.js oids.js defs.js base64.js hex.js int10.js dom.js rfcdef.js test.js tags.js
-  index.css index-dark.css index.js index.html
+  index.css index-dark.css index.js index.html favicon.svg
   README.md LICENSE
   updateOID.sh check.sh
   examples
