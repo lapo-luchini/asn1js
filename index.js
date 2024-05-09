844d85bf9edbcdbb5765d4248d8c3f7a67ad3045
olibu <oliver.burgmaier@gmail.com>
2024-05-09 10:04:10 +0000

    Theme moved into own js file as of performance issues

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const
     area = id('area'),
     file = id('file'),
     examples = id('examples'),
-    selectTheme = id('theme-select'),
     selectDefs = id('definitions'),
     selectTag = id('tags');
 
@@ -157,42 +156,6 @@ for (const [name, onClick] of Object.entries(butClickHandlers)) {
     let elem = id(name);
     if (elem)
         elem.onclick = onClick;
-}
-// set dark theme depending on OS settings
-function setTheme(theme) {
-    if (!selectTheme) {
-        console.log('Themes are currently not working with single file version.');
-        return;
-    }
-    if (theme == 'os') {
-        let prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
-        if (prefersDarkScheme.matches) {
-            theme = 'dark';
-        } else {
-            theme = 'light';
-        }
-    }
-    document.documentElement.style['color-scheme'] = theme
-    document.querySelector('html').setAttribute('data-theme', theme)
-}
-// activate selected theme
-let theme = 'os';
-const localStorageTheme = localStorage.getItem('theme');
-if (localStorageTheme) {
-    theme = localStorageTheme;
-}
-if (theme == 'light') {
-    selectTheme.selectedIndex = 2;
-} else if (theme == 'dark') {
-    selectTheme.selectedIndex = 1;
-}
-setTheme(theme);
-// add handler to theme selction element
-if (selectTheme) {
-    selectTheme.addEventListener ('change', function () {
-        localStorage.setItem('theme', selectTheme.value);
-        setTheme(selectTheme.value);
-    });
 }
 // this is only used if window.FileReader
 function read(f) {
