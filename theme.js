37883bf8c91a7fa05e5020bd87d046b021e8c6d5
olibu <oliver.burgmaier@gmail.com>
2024-05-11 13:30:52 +0000

    theme-color set for iOS devices

--- theme.js
+++ theme.js
@@ -1,37 +1,41 @@
-const selectTheme = document.getElementById('theme-select')
+const selectTheme = document.getElementById('theme-select');
 // set dark theme depending on OS settings
 function setTheme(theme) {
-if (!selectTheme) {
-    console.log('Themes are currently not working with single file version.');
-    return;
-}
-if (theme == 'os') {
-    let prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
-    if (prefersDarkScheme.matches) {
-        theme = 'dark';
-    } else {
-        theme = 'light';
+    if (!selectTheme) {
+        console.log('Themes are currently not working with single file version.');
+        return;
     }
-}
-document.documentElement.style['color-scheme'] = theme
-document.querySelector('html').setAttribute('data-theme', theme)
+    if (theme == 'os') {
+        let prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+        if (prefersDarkScheme.matches) {
+            theme = 'dark';
+        } else {
+            theme = 'light';
+        }
+    }
+    document.documentElement.style['color-scheme'] = theme;
+    document.querySelector('html').setAttribute('data-theme', theme);
+    // set tthe theme-color for iOS devices
+    let bgColor = getComputedStyle(document.documentElement).getPropertyValue('--main-bg-color');
+    let metaThemeColor = document.querySelector('meta[name=theme-color]');
+    metaThemeColor.setAttribute('content', bgColor);
 }
 // activate selected theme
 let theme = 'os';
 const localStorageTheme = localStorage.getItem('theme');
 if (localStorageTheme) {
-theme = localStorageTheme;
+    theme = localStorageTheme;
 }
 if (theme == 'light') {
-selectTheme.selectedIndex = 2;
+    selectTheme.selectedIndex = 2;
 } else if (theme == 'dark') {
-selectTheme.selectedIndex = 1;
+    selectTheme.selectedIndex = 1;
 }
 setTheme(theme);
 // add handler to theme selction element
 if (selectTheme) {
-selectTheme.addEventListener ('change', function () {
-    localStorage.setItem('theme', selectTheme.value);
-    setTheme(selectTheme.value);
-});
+    selectTheme.addEventListener ('change', function () {
+        localStorage.setItem('theme', selectTheme.value);
+        setTheme(selectTheme.value);
+    });
 }
