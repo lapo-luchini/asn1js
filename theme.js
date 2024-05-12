9a0a01e0a462b87ff41381cd65f50f9541efa6d0
Lapo Luchini <lapo@lapo.it>
2024-05-12 11:08:49 +0000

    Small theme refactoring.

--- theme.js
+++ theme.js
@@ -1,10 +1,5 @@
-const selectTheme = document.getElementById('theme-select');
 // set dark theme depending on OS settings
 function setTheme(theme) {
-    if (!selectTheme) {
-        console.log('Themes are currently not working with single file version.');
-        return;
-    }
     if (theme == 'os') {
         let prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
         if (prefersDarkScheme.matches) {
@@ -15,7 +10,7 @@ function setTheme(theme) {
     }
     document.documentElement.style['color-scheme'] = theme;
     document.querySelector('html').setAttribute('data-theme', theme);
-    // set tthe theme-color for iOS devices
+    // set the theme-color for iOS devices
     let bgColor = getComputedStyle(document.documentElement).getPropertyValue('--main-bg-color');
     let metaThemeColor = document.querySelector('meta[name=theme-color]');
     metaThemeColor.setAttribute('content', bgColor);
@@ -26,16 +21,17 @@ const localStorageTheme = localStorage.getItem('theme');
 if (localStorageTheme) {
     theme = localStorageTheme;
 }
-if (theme == 'light') {
-    selectTheme.selectedIndex = 2;
-} else if (theme == 'dark') {
-    selectTheme.selectedIndex = 1;
-}
 setTheme(theme);
-// add handler to theme selction element
+// add handler to theme selection element
+const selectTheme = document.getElementById('theme-select');
 if (selectTheme) {
     selectTheme.addEventListener ('change', function () {
         localStorage.setItem('theme', selectTheme.value);
         setTheme(selectTheme.value);
     });
+    if (theme == 'light') {
+        selectTheme.selectedIndex = 2;
+    } else if (theme == 'dark') {
+        selectTheme.selectedIndex = 1;
+    }
 }
