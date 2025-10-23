b870248183634ab8d68311152fe54eabf746607b
Lapo Luchini <lapo@lapo.it>
2025-10-23 13:48:26 +0000

    Add timestamp request example. (partial alternative to GitHub PR #96)

--- defs.js
+++ defs.js
@@ -140,4 +140,5 @@ Defs.commonTypes = [
     [ 'PKCS#10 certification request', '1.2.840.113549.1.10.1.1', 'CertificationRequest' ],
     [ 'CMP PKI Message', '1.3.6.1.5.5.7.0.16', 'PKIMessage' ],
     [ 'LDAP Message', '1.3.6.1.1.18', 'LDAPMessage' ],
+    [ 'Time Stamp Request', '1.3.6.1.5.5.7.0.13', 'TimeStampReq' ],
 ].map(arr => ({ description: arr[0], ...Defs.moduleAndType(rfcdef[arr[1]], arr[2]) }));
